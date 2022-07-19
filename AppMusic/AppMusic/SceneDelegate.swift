ea13aeefadfe771f571eaa07875b73f8253a4651
OanhNunu <ntoanh1997@gmail.com>
2022-07-19 09:44:45 +0700

    create music app

---
+++ AppMusic/AppMusic/SceneDelegate.swift
@@ -0,0 +1,49 @@
+//
+//  SceneDelegate.swift
+//  AppMusic
+//
+//  Created by Nguyen Thi Oanh on 09/07/2022.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
+    var window: UIWindow?
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        
+        guard let windowscene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: windowscene)
+        self.window = window
+        
+        App.shared.window = window
+        App.shared.startInterface()
+        ///let viewController = TabBarController()
+        ///let navi = UINavigationController(rootViewController: viewController)
+        ///navi.isNavigationBarHidden = true
+        ///window.rootViewController = navi
+        
+    }
+    
+    func sceneDidDisconnect(_ scene: UIScene) {
+        
+    }
+    
+    func sceneDidBecomeActive(_ scene: UIScene) {
+        
+    }
+    
+    func sceneWillResignActive(_ scene: UIScene) {
+        
+    }
+    
+    func sceneWillEnterForeground(_ scene: UIScene) {
+        
+    }
+    
+    func sceneDidEnterBackground(_ scene: UIScene) {
+        
+    }
+    
+}
